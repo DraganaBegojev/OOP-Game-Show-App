9944344ba6a9fc2e0fec1bfe2c5efba4749eab3b
DraganaBegojev <163642152+DraganaBegojev@users.noreply.github.com>
2025-05-06 12:26:26 +0200

    Game and Phrase classes
    Declaring the Game class and the Phrase class.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+// phrase instance
+const phrase = new Phrase('yay for treehouse fsjs techdegree!');
+
+// game instance
+const game = new Game();    
