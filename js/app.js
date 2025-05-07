898a4425ee8488d8253a5d401c7f7ef92e0ac18d
DraganaBegojev <163642152+DraganaBegojev@users.noreply.github.com>
2025-05-07 11:12:04 +0200

    Initialize the game
    Created startButton.addEventListener which initialize the game when the start button is clicked.

--- js/app.js
+++ js/app.js
@@ -2,3 +2,11 @@
  * Project 4 - OOP Game App
  * app.js */
 
+let game;
+const startButton = document.getElementById('btn__reset');
+
+// initialize the game when the start button is clicked
+startButton.addEventListener('click', () => {
+    game = new Game();
+    game.startGame();
+});
