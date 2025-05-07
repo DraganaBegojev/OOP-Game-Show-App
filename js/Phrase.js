9882b606a914114dc5649797cb312e4c772ae63e
DraganaBegojev <163642152+DraganaBegojev@users.noreply.github.com>
2025-05-07 10:15:32 +0200

    Created addPhraseToDisplay() method
    This method adds letter placeholders to the display when the game starts.

--- js/Phrase.js
+++ js/Phrase.js
@@ -6,4 +6,18 @@ class Phrase {
   constructor(phrase) {
     this.phrase = phrase.toLowerCase();
   }
+  addPhraseToDisplay() {
+    const phraseContainer = document.querySelector('#phrase ul');
+    const phraseArray = this.phrase.split('');
+    phraseArray.forEach((char) => {
+      const li = document.createElement('li');
+      li.textContent = char;
+      if (char === ' ') {
+        li.className = 'space';
+      } else {
+        li.classList.add('hide', 'letter', char);
+      }
+      phraseContainer.appendChild(li);
+    });
+  }
 }
